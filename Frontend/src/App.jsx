50bc932d822f720a37138721949b3679f2805b35
Ankushdhull9908 <123265836+Ankushdhull9908@users.noreply.github.com>
2024-10-22 22:03:12 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await fetch('http://localhost:5000/submit', {
+            const res = await fetch('https://formdeploytest.onrender.com/submit', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
